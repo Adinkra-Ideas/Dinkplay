938eb92be63983d8b3c4e9282f72e6e3e2297b31
Adinkra-Ideas <musicjeejo@gmail.com>
2025-01-02 00:09:09 +0100

    Bluetooth, System, Other App Interrupts properly handled

--- iOS/src/MyMPRemoteCC.mm
+++ iOS/src/MyMPRemoteCC.mm
@@ -2,7 +2,7 @@
 #import <MediaPlayer/MediaPlayer.h>
 #import <UIKit/UIKit.h>
 #include <stdio.h> // because nslog aint printing to qt debug
-
+#import <unistd.h>
 #import "MyMPRemoteCC.h"
 
 // Un-initialized pointer to cpp class.
@@ -20,6 +20,7 @@ Top *cppObject;
   */
 @interface MyMPRemoteCC : NSObject
 {
+  NSNotificationCenter *notifCenter;        // for adding and removing observers AKA notification for audio state-change listeners
   MPRemoteCommandCenter *myCommandCenter;   // For holding the control center instance
   MPNowPlayingInfoCenter *myPlayingCenter;
 
@@ -38,12 +39,32 @@ Top *cppObject;
 // class constructor (id means instancetype AKA MyMPRemoteCC)
 -(id)init
 {
+  // Initialize base class before this AKA self
+  // IF PROBLEM, SIMPLY COMMENT OUT COZ APP WORKED FINE WITHOUT IT.
+  self = [super init];
+  if (!self) {
+    return nil;
+  }
+
+  // printf("%s", "bbbbbbbbbbbbbbbbb!!!\n");
+  // // Adding observers AKA listeners for audio state-changes.
+  // // Listen for and call method specified in "selector"
+  // notifCenter = [NSNotificationCenter defaultCenter];
+  // [notifCenter  addObserver:
+  //               self
+  //               selector:@selector(onAudioSessionInterrupted:) //wow! if the callback method takes an arg and you fail to add the appending ":" to its name, app will crash at runtime when it gets here
+  //               name:AVAudioSessionInterruptionNotification
+  //               object:[AVAudioSession sharedInstance]];
+
   return self;
 }
 
 // class destructor
 -(void)dealloc
 {
+  // unplug the listeners.  we will comment this
+  [notifCenter removeObserver:self];
+
   [super dealloc];  // call the destructor of NSObject AKA parent that we inherited from
 }
 
@@ -70,14 +91,29 @@ Top *cppObject;
   * @returns void
   */
 - (void)listenForInterruptions {
+  [[AVAudioSession sharedInstance] setCategory:
+                  AVAudioSessionCategoryPlayback
+                  withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker | AVAudioSessionCategoryOptionAllowBluetooth | AVAudioSessionCategoryOptionAllowBluetoothA2DP | AVAudioSessionCategoryOptionAllowAirPlay
+                  error:nil];
+
   @try {
     // Listen for system interrupts and duckings and suspend/unsuspend play accordingly
-    NSNotificationCenter *notifCenter = [NSNotificationCenter defaultCenter];
+    notifCenter = [NSNotificationCenter defaultCenter];
     [notifCenter  addObserver:
                   self
                   selector:@selector(onAudioSessionInterrupted:) //wow! if the callback method takes an arg and you fail to add the appending ":" to its name, app will crash at runtime when it gets here
                   name:AVAudioSessionInterruptionNotification
                   object:[AVAudioSession sharedInstance]];
+    //The below is for appending listener for bluetooth connection.
+    // if you fail to add "withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker | AVAudioSessionCategoryOptionAllowBluetoothA2DP | AVAudioSessionCategoryOptionAllowAirPlay"
+    // beforenow, it will not fire properly besides firng on app launch.
+    [notifCenter  addObserver:
+                  self
+                  selector:@selector(onAudioSessionInterrupted:)
+                  name:AVAudioSessionRouteChangeNotification
+                  object:[AVAudioSession sharedInstance]];
+
+
   }
   @catch (NSException *exception) {
     // NSLog(@"%@", exception);
@@ -96,25 +132,54 @@ Top *cppObject;
   */
 - (void) onAudioSessionInterrupted: (NSNotification *) notification
 {
-    //Check the type of notification, especially if you are sending multiple AVAudioSession events here
-    printf("%s", "Interruption RECEIVED!!!\n");
+  // This means it is either a system interrupt or ducking by other audio apps
+  if ([notification.name isEqualToString:AVAudioSessionInterruptionNotification]) {
+    NSDictionary *userInfo = notification.userInfo;
 
-    if ([notification.name isEqualToString:AVAudioSessionInterruptionNotification]) {
-        printf("%s", "Interruption confirmed\n");
-
-        //Check to see if it was a Begin interruption
-        if ([[notification.userInfo valueForKey:AVAudioSessionInterruptionTypeKey] isEqualToNumber:[NSNumber numberWithInt:AVAudioSessionInterruptionTypeBegan]]) {
-            printf("%s", "Interruption begin\n");
-            //Suspend your audio
-            cppObject->suspendAudio();
-
-        } else if([[notification.userInfo valueForKey:AVAudioSessionInterruptionTypeKey] isEqualToNumber:[NSNumber numberWithInt:AVAudioSessionInterruptionTypeEnded]]){
-            printf("%s", "Interruption end\n");
-            cppObject->unsuspendAudio();
-
-        }
+    //Check to see if it was a Begin interruption
+    if ([[userInfo valueForKey:AVAudioSessionInterruptionTypeKey] isEqualToNumber:[NSNumber numberWithInt:AVAudioSessionInterruptionTypeBegan]]) {
+      // Suspend your audio
+      cppObject->suspendAudio();
+    } else if([[userInfo valueForKey:AVAudioSessionInterruptionTypeKey] isEqualToNumber:[NSNumber numberWithInt:AVAudioSessionInterruptionTypeEnded]]){
+      // This block is intended to resume play only if it is ok to do so.
+      // AKA system interrupts can resume, but docking by z.B VLC shouldnt
+      // because if it resumes after vlc ducked it, it will lose audio and the
+      // only work around in that case is to wait for at least 6 seconds after
+      // ducking interrupt ends before resuming. Even at that, it still wont
+      // resume from background. That is why iOS system forbids it.
+      NSNumber *optionsValue = userInfo[AVAudioSessionInterruptionOptionKey];
+      AVAudioSessionInterruptionOptions options = (AVAudioSessionInterruptionOptions)[optionsValue unsignedIntegerValue];
+      if (options & AVAudioSessionInterruptionOptionShouldResume) {
+        // Unsuspend your audio
+        cppObject->unsuspendAudio();
+      }
     }
+  }
+  // This means a headset device (bluetooth or usb audio to speaker) interruption
+  else if ([notification.name isEqualToString:AVAudioSessionRouteChangeNotification]) {
+    NSDictionary *userInfo = notification.userInfo;
+    NSNumber *reasonValue = userInfo[AVAudioSessionRouteChangeReasonKey];
+
+    if (reasonValue) {
+      AVAudioSessionRouteChangeReason reason = (AVAudioSessionRouteChangeReason)[reasonValue unsignedIntegerValue];
+      switch (reason) {
+        case AVAudioSessionRouteChangeReasonNewDeviceAvailable:
+          // plugging in a headset, has made a preferred audio route available
+          // Unsuspend your audio
+          cppObject->unsuspendAudio();
+          break;
+        case AVAudioSessionRouteChangeReasonOldDeviceUnavailable:
+          // unplugging a headset, now users audio privacy must be respected
+          // Suspend your audio
+          cppObject->suspendAudio();
+          break;
+        default:
+          break;
+      }
+    }
+  }
 }
+
 
 - (void)setupMPRemoteCommandCenter {
   // This line was used with the old ApI.
@@ -143,7 +208,7 @@ Top *cppObject;
   //
   [myCommandCenter.pauseCommand setEnabled:YES];
   [myCommandCenter.pauseCommand addTarget:self action:@selector(pauseAudio:)];
-  // Play and pause buttons (earphone control)
+  // Play and pause buttons (bluetooth earphone control)
   [myCommandCenter.togglePlayPauseCommand setEnabled:YES];
   [myCommandCenter.togglePlayPauseCommand addTarget:self action:@selector(playPauseAudio:)];
 
